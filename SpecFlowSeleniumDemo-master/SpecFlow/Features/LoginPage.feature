59e22ee1f237727c2b7a46cf28b85a5bbcbc84f9
Adam Blower <adam.blower@tpg.local>
2019-04-12 17:31:01 +0100

    First commit

---
+++ SpecFlowSeleniumDemo-master/SpecFlow/Features/LoginPage.feature
@@ -0,0 +1,7 @@
+﻿Feature: LoginPage
+
+@LoginPage
+Scenario: Login button exists
+	Given I have navigated to my test site
+	When the login page loads
+	Then the login button should display on the page
---
+++ SpecFlowSeleniumDemo-master/SpecFlow/Steps/LoginPageSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.Steps
+{
+    [Binding]
+    public class LoginPageSteps
+    {
+        [When(@"the Login page loads")]
+        public void WhenTheLoginPageLoads()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the login button should display on the page")]
+        public void ThenTheLoginButtonShouldDisplayOnThePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
